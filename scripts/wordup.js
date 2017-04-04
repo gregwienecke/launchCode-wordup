2a40497b2074dfe084db5cc73d67031cee978cd4
Greg Wienecke <gregwienecke@gmail.com>
2017-04-04 12:07:19 -0500

    Implement wordScore function (19)

--- scripts/wordup.js
+++ scripts/wordup.js
@@ -74,22 +74,36 @@ function checkIfWordIsReal(word) {
     // make an AJAX call to the Pearson API
     $.ajax({
         // TODO 13 what should the url be?
-        url: "www.todo13.com",
+        url: "http://api.pearson.com/v2/dictionaries/lasde/entries?headword=" + word,
         success: function(response) {
-            console.log("We received a response from Pearson!");
+            //console.log("We received a response from Pearson!");
 
             // let's print the response to the console so we can take a looksie
-            console.log(response);
+            //console.log(response.results);
 
             // TODO 14
             // Replace the 'true' below.
             // If the response contains any results, then the word is legitimate.
             // Otherwise, it is not.
-            var theAnswer = true;
+            var theAnswer = false;    
+            if (response.results.length > 0){
+                theAnswer = true;
+            }
+            
 
             // TODO 15
             // Update the corresponding wordSubmission in the model
-
+            // model.wordSubmissions is an array of objects. Each object has a word property with 
+            // the submitted word. Loop through the array and find the one with the matching value
+            // and add a isRealWord property set to theAnswer (true or false)
+            console.log(model.wordSubmissions);
+            for (var i = 0; i < model.wordSubmissions.length; i++){
+                if (word == model.wordSubmissions[i].word){
+                    var correspondingWord = model.wordSubmissions[i];
+                    correspondingWord['isRealWord'] = theAnswer;
+                    //console.log(correspondingWord);
+                };
+            }
 
             // re-render
             render();
@@ -115,7 +129,7 @@ function render() {
 
     // TODO 2
     // Update the curent time remaining on the scoreboard.
-
+    $("#time-remaining").text(model.secondsRemaining);
 
     // if the game has not started yet, just hide the #game container and exit
     if (model.gameHasStarted == false) {
@@ -130,24 +144,28 @@ function render() {
     $("#word-submissions").empty();
     // TODO 10
     // Add a few things to the above code block (underneath "// clear stuff").
-
+    $('#textbox').removeClass('bad-attempt');
+    $('.disallowed-letter').remove();
+    $('#textbox').attr('disabled', false);
 
     // reveal the #game container
     $("#game").show();
 
     // render the letter tiles
-    var letterChips = model.allowedLetters.map(letterChip)
+    var letterChips = model.allowedLetters.map(letterChip);
     $("#allowed-letters").append(letterChips);
 
     // TODO 11
     // Render the word submissions
+    var wordChips = model.wordSubmissions.map(wordSubmissionChip);
+    $('#word-submissions').append(wordChips);
 
 
     // Set the value of the textbox
     $("#textbox").val(model.currentAttempt);
     // TODO 3
     // Give focus to the textbox.
-
+    $("#textbox").focus();
 
     // if the current word attempt contains disallowed letters,
     var disallowedLetters = disallowedLettersInWord(model.currentAttempt);
@@ -160,7 +178,10 @@ function render() {
 
         // TODO 8
         // append the red letter chips to the form
-
+        for (var i = 0; i < redLetterChips.length; i++){
+            $('#word-attempt-form').append(redLetterChips[i]);    
+        }
+        
     }
 
     // if the game is over
@@ -168,6 +189,7 @@ function render() {
     if (gameOver) {
         // TODO 9
         // disable the text box and clear its contents
+        $('#textbox').attr('disabled', true).val('')
 
     }
 }
@@ -205,13 +227,22 @@ function wordSubmissionChip(wordSubmission) {
         var scoreChip = $("<span></span>").text("⟐");
         // TODO 17
         // give the scoreChip appropriate text content
+        var score = wordScore(wordSubmission.word);
+        scoreChip
+            .text(score)
+            .attr('class', 'tag tag-sm bg-primary');
 
+        if (wordSubmission.isRealWord == false){
+            scoreChip
+                .text('X')
+                .attr('class', 'tag tag-sm bg-danger');
+        };
         // TODO 18
         // give the scoreChip appropriate css classes
 
         // TODO 16
         // append scoreChip into wordChip
-
+        wordChip.append(scoreChip);
     }
 
     return wordChip;
@@ -242,7 +273,11 @@ $(document).ready(function() {
     // Add another event handler with a callback function.
     // When the textbox content changes,
     // update the .currentAttempt property of the model and re-render
-
+    $('#textbox').on('input', function(){
+        model.currentAttempt = $('#textbox').val();
+        render();
+        //console.log( $('#textbox').val() );
+    });
 
     // when the form is submitted
     $("#word-attempt-form").submit(function(evt) {
@@ -280,7 +315,12 @@ function isDisallowedLetter(letter) {
     // TODO 7
     // This should return true if the letter is not an element of
     // the .allowedLetters list in the model
-    return false;
+    if (model.allowedLetters.indexOf(letter) >= 0){
+        return false;    
+    } else {
+        return true;
+    }
+    
 }
 
 /**
@@ -299,6 +339,11 @@ function disallowedLettersInWord(word) {
 function containsOnlyAllowedLetters(word) {
     // TODO 12
     // Return the actual answer.
+    for (var i = 0; i < word.length; i++){
+        if (model.allowedLetters.indexOf(word[i]) < 0){
+            return false;
+        };
+    }
     return true;
 }
 
@@ -330,7 +375,9 @@ function wordScore(word) {
     // TODO 19
     // Replace the empty list below.
     // Map the list of letters into a list of scores, one for each letter.
-    var letterScores = [];
+    var letterScores = letters.map(function(letter){
+        return letterScore(letter);
+    });
 
     // return the total sum of the letter scores
     return letterScores.reduce(add, 0);
